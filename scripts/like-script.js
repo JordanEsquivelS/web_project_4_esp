4d0629d56e9cda5a9303cea58635ae8d1d31de35
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-03-22 10:47:30 -0500

    Modificar para git pages

--- scripts/like-script.js
+++ scripts/like-script.js
@@ -3,9 +3,9 @@ let heartIcons = document.querySelectorAll(".photo-grid__like");
 heartIcons.forEach(function (heartIcon) {
   heartIcon.addEventListener("click", function () {
     if (heartIcon.src.includes("/images/corazon_blanco.svg")) {
-      heartIcon.src = "/images/corazon_negro.svg";
+      heartIcon.src = "images/corazon_negro.svg";
     } else {
-      heartIcon.src = "/images/corazon_blanco.svg";
+      heartIcon.src = "images/corazon_blanco.svg";
     }
   });
 });
