b12c15c460ec99c18de7ff08ec869e849920dc0e
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-03-22 01:43:38 -0500

    Agregar photo-grid, compatibilidad pantalla y like

---
+++ scripts/like-script.js
@@ -0,0 +1,11 @@
+let heartIcons = document.querySelectorAll(".photo-grid__like");
+
+heartIcons.forEach(function (heartIcon) {
+  heartIcon.addEventListener("click", function () {
+    if (heartIcon.src.includes("/images/corazon_blanco.svg")) {
+      heartIcon.src = "/images/corazon_negro.svg";
+    } else {
+      heartIcon.src = "/images/corazon_blanco.svg";
+    }
+  });
+});
