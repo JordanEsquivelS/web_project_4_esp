6d3deda0f128c0176463650f254f427e13bd7d27
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-03-23 09:51:03 -0500

    Agregar Validacion de input

--- scripts/popup-scripts.js
+++ scripts/popup-scripts.js
@@ -1,11 +1,11 @@
-let popup = document.querySelector(".popup");
-let edit = document.querySelector(".profile-info__edit");
-let close = document.querySelector(".popup__container-image");
-let guardar = document.querySelector(".form__guardar");
-let nameInput = document.querySelector(".form__name");
-let aboutMeInput = document.querySelector(".form__about-me");
-let nombre = document.querySelector(".profile-info__nombre");
-let aboutMe = document.querySelector(".profile-info__about-me");
+const popup = document.querySelector(".popup");
+const edit = document.querySelector(".profile-info__edit");
+const close = document.querySelector(".popup__container-image");
+const guardar = document.querySelector(".form__guardar");
+const nameInput = document.querySelector(".form__name");
+const aboutMeInput = document.querySelector(".form__about-me");
+const nombre = document.querySelector(".profile-info__nombre");
+const aboutMe = document.querySelector(".profile-info__about-me");
 
 // Mostrar popup y llenar inputs con información actual
 edit.addEventListener("click", function (event) {
@@ -18,9 +18,13 @@ edit.addEventListener("click", function (event) {
 // Actualizar información y ocultar popup
 guardar.addEventListener("click", function (event) {
   event.preventDefault(); // previene el comportamiento por defecto del botón submit
-  nombre.textContent = nameInput.value;
-  aboutMe.textContent = aboutMeInput.value;
-  popup.style.display = "none";
+  if (document.querySelector(".form").checkValidity()) {
+    nombre.textContent = nameInput.value;
+    aboutMe.textContent = aboutMeInput.value;
+    popup.style.display = "none";
+  } else {
+    alert("Por favor completa los campos requeridos.");
+  }
 });
 
 // Cerrar popup sin actualizar información
