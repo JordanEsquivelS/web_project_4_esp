5068c33f0ee1fbaf423e9db8078402640671f357
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-04-05 17:47:40 -0500

    Cambiar validacion y agergar nombres a funciones

--- scripts/popupNuevoLugar.js
+++ scripts/popupNuevoLugar.js
@@ -2,28 +2,29 @@ const lugarAgregar = document.querySelector(".profile__agregar");
 const popupNuevoLugar = document.querySelector("#nuevo-lugar");
 const form = popupNuevoLugar.querySelector("form");
 
-// Agregar un evento de click al botón "Agregar"
-lugarAgregar.addEventListener("click", () => {
+function abrirPopupLugar() {
   popupNuevoLugar.classList.add("open");
-});
+}
+
+// Agregar un evento de click al botón "Agregar"
+lugarAgregar.addEventListener("click", abrirPopupLugar);
+
+function cerrarPopupLugar() {
+  popupNuevoLugar.classList.remove("open");
+  form.reset();
+}
 
 // Agregar un evento de click al botón "Cerrar" dentro de la ventana emergente
 const cerrarLugar = document.querySelector("#cerrarLugar");
-cerrarLugar.addEventListener("click", () => {
-  popupNuevoLugar.classList.remove("open");
-  form.reset();
-});
+cerrarLugar.addEventListener("click", cerrarPopupLugar);
 
 let nextId = 1;
 
-// Agregar un evento de envío al formulario dentro de la ventana emergente
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
+function agregarNuevaTarjeta() {
   // Obtener los valores de los campos del formulario
   const titulo = form.querySelector("#titulo").value;
   const link = form.querySelector("#form__input-url").value;
-  if (titulo.value === "" || link.value === "") {
+  if (!titulo || !link) {
     alert("Por favor ingrese todos los datos.");
     return false;
   }
@@ -58,45 +59,58 @@ form.addEventListener("submit", (evt) => {
 
   // Aplicar el evento de clic a todas las imágenes
   agregarEventoLike();
-});
+
+  // Aplicar el evento de clic para eliminar tarjeta
+  agregarEventoEliminarTarjeta();
+}
+
+form.addEventListener("submit", agregarNuevaTarjeta);
+
 function agregarEventoLike() {
   let heartIcons = document.querySelectorAll(".photo-grid__like");
 
+  function toggleHeartIcon() {
+    if (this.src.includes("/images/corazon_blanco.svg")) {
+      this.src = "images/corazon_negro.svg";
+    } else {
+      this.src = "images/corazon_blanco.svg";
+    }
+  }
+
   heartIcons.forEach(function (heartIcon) {
-    heartIcon.addEventListener("click", function () {
-      if (heartIcon.src.includes("/images/corazon_blanco.svg")) {
-        heartIcon.src = "images/corazon_negro.svg";
-      } else {
-        heartIcon.src = "images/corazon_blanco.svg";
-      }
-    });
+    heartIcon.addEventListener("click", toggleHeartIcon);
   });
+}
+
+function agregarEventoEliminarTarjeta() {
+  function eliminarTarjeta() {
+    let card = this.parentNode;
+    let id = card.getAttribute("id");
+
+    // Buscar la tarjeta con el id correspondiente en initialCards
+    let tarjetaAEliminar = initialCards.find(function (tarjeta) {
+      return tarjeta.id === id;
+    });
+
+    // Eliminar la tarjeta del array initialCards
+    let index = initialCards.indexOf(tarjetaAEliminar);
+    if (index !== -1) {
+      initialCards.splice(index, 1);
+    }
+
+    // Actualizar la cuadrícula de fotos en el archivo cards-images.js
+    const photoGridContainer = document.getElementById("grid-container");
+    photoGridContainer.removeChild(card);
+  }
 
   // Agregar un event listener a cada botón "photo-grid__delete"
   let deleteButtons = document.querySelectorAll(".photo-grid__delete");
 
   deleteButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      let card = button.parentNode;
-      let id = card.getAttribute("id");
-
-      // Buscar la tarjeta con el id correspondiente en initialCards
-      let tarjetaAEliminar = initialCards.find(function (tarjeta) {
-        return tarjeta.id === id;
-      });
-
-      // Eliminar la tarjeta del array initialCards
-      let index = initialCards.indexOf(tarjetaAEliminar);
-      if (index !== -1) {
-        initialCards.splice(index, 1);
-      }
-
-      // Actualizar la cuadrícula de fotos en el archivo cards-images.js
-      const photoGridContainer = document.getElementById("grid-container");
-      photoGridContainer.removeChild(card);
-    });
+    button.addEventListener("click", eliminarTarjeta);
   });
 }
 
 // Llama a la función una vez para aplicar el evento de clic a las imágenes existentes
 agregarEventoLike();
+agregarEventoEliminarTarjeta();
