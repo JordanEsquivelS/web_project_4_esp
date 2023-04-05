b6b71c13cf79296ae7485e49627f87d860ab2009
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-04-05 18:56:20 -0500

    Agregar variable que faltaba

--- scripts/popupImagenGrande.js
+++ scripts/popupImagenGrande.js
@@ -1,4 +1,4 @@
-
+const photoGridContainer = document.getElementById("grid-container");
 const popupImagen = document.querySelector(".popImg");
 const imagenGrande = document.querySelector(".popImg__imagenGrande");
 const textoImagenGrande = document.querySelector(".popImg__text");
@@ -6,7 +6,9 @@ const cerrarImagen = document.querySelector(".popImg__cerrar");
 
 photoGridContainer.addEventListener("click", (event) => {
   if (event.target.classList.contains("photo-grid__image")) {
-    const titulo = event.target.closest(".photo-grid").querySelector(".photo-grid__text").textContent;
+    const titulo = event.target
+      .closest(".photo-grid")
+      .querySelector(".photo-grid__text").textContent;
     const src = event.target.getAttribute("src");
 
     imagenGrande.setAttribute("src", src);
