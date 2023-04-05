6080e26230476f19dc765403bd78a47066b3bb4f
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-04-05 17:02:27 -0500

    Agregar biblioteca de notificacion y validacion datos

--- scripts/popupProfile.js
+++ scripts/popupProfile.js
@@ -1,37 +1,49 @@
 const popup = document.querySelector(".popup");
-const edit = document.querySelector(".profile-info__edit");
-const close = document.querySelector(".popup__container-image");
-const guardar = document.querySelector(".form__guardar");
+const editButton = document.querySelector(".profile-info__edit");
+const closePopupButton = document.querySelector(".popup__container-image");
+const saveButton = document.querySelector(".form__guardar");
 const nameInput = document.querySelector(".form__name");
 const aboutMeInput = document.querySelector("#form__input-aboutMe");
 const nombre = document.querySelector(".profile-info__nombre");
 const aboutMe = document.querySelector(".profile-info__about-me");
+const soloLetras = /^[a-zA-Z\s]+$/;
 
 // Mostrar popup y llenar inputs con información actual
-edit.addEventListener("click", function (event) {
+function editarDatos(event) {
   event.preventDefault();
   popup.classList.add("open");
-  /* popup.style.display = "block";*/
   nameInput.value = nombre.textContent;
   aboutMeInput.value = aboutMe.textContent;
-});
+}
+editButton.addEventListener("click", editarDatos);
 
 // Actualizar información y ocultar popup
-guardar.addEventListener("click", function (event) {
+function guardarDatos(event) {
   event.preventDefault(); // previene el comportamiento por defecto del botón submit
-  if (document.querySelector(".form").checkValidity()) {
-    nombre.textContent = nameInput.value;
-    aboutMe.textContent = aboutMeInput.value;
-    popup.classList.remove("open");
-    /*popup.style.display = "none";*/
+  if (nameInput.value.trim() === "" || aboutMeInput.value.trim() === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor completa los campos requeridos.",
+    });
+  } else if (!soloLetras.test(nameInput.value.trim())) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Asegúrate de que el campo de nombre solo contenga letras.",
+    });
   } else {
-    alert("Por favor completa los campos requeridos.");
+    nombre.textContent = nameInput.value.trim();
+    aboutMe.textContent = aboutMeInput.value.trim();
+    popup.classList.remove("open");
   }
-});
+}
+
+saveButton.addEventListener("click", guardarDatos);
 
 // Cerrar popup sin actualizar información
-close.addEventListener("click", function () {
-  /*popup.style.display = "none";*/
+function closePopup(event) {
   popup.classList.remove("open");
-});
+}
 
+closePopupButton.addEventListener("click", closePopup);
