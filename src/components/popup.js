11e0c6fc8ecb714fd299f640504cc6dd85730e70
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-06-21 09:58:03 -0500

    Corregir tecla scape

--- src/components/popup.js
+++ src/components/popup.js
@@ -2,18 +2,16 @@ class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._closeButton = this._popup.querySelector(".popup__container-image");
-
-    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add("open");
-    document.addEventListener("keydown", this._handleEscClose);
+    document.addEventListener("keydown", this._handleEscClose.bind(this));
   }
 
   close() {
     this._popup.classList.remove("open");
-    document.removeEventListener("keydown", this._handleEscClose);
+    document.removeEventListener("keydown", this._handleEscClose.bind(this));
   }
 
   _handleEscClose(event) {
